846696027f47f8e98b0bd2766a421c8f3b4dee62
zhinanw@andrew.cmu.edu <zhinanwang@znbuchadoudembp.wifi.local.cmu.edu>
2023-01-25 14:01:15 -0500

    resolve issue with incorrect exports

--- src/logger.js
+++ src/logger.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.monitorConfig = exports.io = exports.io_close = exports.io_one = exports.prepare_io_string = exports.init = exports.setup = exports.setup_one = exports.setup_one_log = exports.close = exports.open = exports.express_open = exports.expressLogger = void 0;
 const express_1 = __importDefault(require("express"));
 /*
  * Logger module: ability to dynamically turn on/off logging for http requests & socket.io events
@@ -29,183 +30,193 @@ const opts = {
     },
 };
 /* -- Logger -- */
-const Logger = {
-    init: function (app) {
-        opts.express.app = app;
-        /* Open log file stream & initialize express logging if meta.config.logger* variables are set */
-        Logger.setup();
-    },
-    setup: function () {
+function expressLogger(req, res, next) {
+    /*
+        * The new express.logger
+        *
+        * This hijack allows us to turn logger on/off dynamically within express
+        */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (meta_1.default.config.loggerStatus > 0) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        Logger.setup_one('loggerPath', meta_1.default.config.loggerPath);
-    },
-    setup_one: function (key, value) {
-        /*
-         * 1. Open the logger stream: stdout or file
-         * 2. Re-initialize the express logger hijack
-         */
-        if (key === 'loggerPath') {
-            Logger.setup_one_log(value);
-            Logger.express_open();
-        }
-    },
-    setup_one_log: function (value) {
-        /*
-         * If logging is currently enabled, create a stream.
-         * Otherwise, close the current stream
-         */
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (meta_1.default.config.loggerStatus > 0 || meta_1.default.config.loggerIOStatus) {
-            const stream = Logger.open(value);
-            if (stream) {
-                opts.streams.log.f = stream;
-            }
-            else {
-                opts.streams.log.f = process.stdout;
+        return opts.express.ofn(req, res, next);
+    }
+    return next();
+}
+exports.expressLogger = expressLogger;
+function express_open() {
+    if (opts.express.set !== 1) {
+        opts.express.set = 1;
+        opts.express.app.use(expressLogger);
+    }
+    /*
+        * Always initialize "ofn" (original function) with the original logger function
+        */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    opts.express.ofn = morgan('combined', { stream: opts.streams.log.f });
+}
+exports.express_open = express_open;
+function open(value) {
+    /* Open the streams to log to: either a path or stdout */
+    let stream;
+    if (value) {
+        if (file_1.default.existsSync(value)) {
+            const stats = fs.statSync(value);
+            if (stats) {
+                if (stats.isDirectory()) {
+                    stream = fs.createWriteStream(path.join(value, 'nodebb.log'), { flags: 'a' });
+                }
+                else {
+                    stream = fs.createWriteStream(value, { flags: 'a' });
+                }
             }
         }
         else {
-            Logger.close(opts.streams.log);
+            stream = fs.createWriteStream(value, { flags: 'a' });
         }
-    },
-    open: function (value) {
-        /* Open the streams to log to: either a path or stdout */
+        if (stream) {
+            stream.on('error', (err) => {
+                winston.error(err.stack);
+            });
+        }
+    }
+    else {
+        stream = process.stdout;
+    }
+    return stream;
+}
+exports.open = open;
+function close(stream) {
+    if (stream.f !== process.stdout && stream.f) {
         let stream;
-        if (value) {
-            if (file_1.default.existsSync(value)) {
-                const stats = fs.statSync(value);
-                if (stats) {
-                    if (stats.isDirectory()) {
-                        stream = fs.createWriteStream(path.join(value, 'nodebb.log'), { flags: 'a' });
-                    }
-                    else {
-                        stream = fs.createWriteStream(value, { flags: 'a' });
-                    }
-                }
-            }
-            else {
-                stream = fs.createWriteStream(value, { flags: 'a' });
-            }
-            if (stream) {
-                stream.on('error', (err) => {
-                    winston.error(err.stack);
-                });
-            }
+        stream.end();
+    }
+    stream.f = null;
+}
+exports.close = close;
+function setup_one_log(value) {
+    /*
+        * If logging is currently enabled, create a stream.
+        * Otherwise, close the current stream
+        */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (meta_1.default.config.loggerStatus > 0 || meta_1.default.config.loggerIOStatus) {
+        const stream = open(value);
+        if (stream) {
+            opts.streams.log.f = stream;
         }
         else {
-            stream = process.stdout;
+            opts.streams.log.f = process.stdout;
         }
-        return stream;
-    },
-    close: function (stream) {
-        if (stream.f !== process.stdout && stream.f) {
-            let stream;
-            stream.end();
-        }
-        stream.f = null;
-    },
-    monitorConfig: function (socket, data) {
-        /*
-         * This monitor's when a user clicks "save" in the Logger section of the admin panel
-         */
-        Logger.setup_one(data.key, data.value);
-        Logger.io_close(socket);
-        Logger.io(socket);
-    },
-    express_open: function () {
-        if (opts.express.set !== 1) {
-            opts.express.set = 1;
-            opts.express.app.use(Logger.expressLogger);
-        }
-        /*
-         * Always initialize "ofn" (original function) with the original logger function
-         */
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        opts.express.ofn = morgan('combined', { stream: opts.streams.log.f });
-    },
-    expressLogger: function (req, res, next) {
-        /*
-         * The new express.logger
-         *
-         * This hijack allows us to turn logger on/off dynamically within express
-         */
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (meta_1.default.config.loggerStatus > 0) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            return opts.express.ofn(req, res, next);
-        }
-        return next();
-    },
-    prepare_io_string: function (_type, _uid, _args) {
-        /*
-         * This prepares the output string for intercepted socket.io events
-         *
-         * The format is: io: <uid> <event> <args>
-         */
-        try {
-            return `io: ${_uid} ${_type} ${util.inspect(Array.prototype.slice.call(_args), { depth: 3 })}\n`;
-        }
-        catch (err) {
-            winston.info('Logger.prepare_io_string: Failed', err);
-            return 'error';
-        }
-    },
-    io_close: function (socket) {
-        /*
-         * Restore all hijacked sockets to their original emit/on functions
-         */
-        if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
-            return;
-        }
-        const clientsMap = socket.io.sockets.sockets;
-        for (const [, client] of clientsMap) {
-            if (client.oEmit && client.oEmit !== client.emit) {
-                client.emit = client.oEmit;
+    }
+    else {
+        close(opts.streams.log);
+    }
+}
+exports.setup_one_log = setup_one_log;
+function setup_one(key, value) {
+    /*
+        * 1. Open the logger stream: stdout or file
+        * 2. Re-initialize the express logger hijack
+        */
+    if (key === 'loggerPath') {
+        setup_one_log(value);
+        express_open();
+    }
+}
+exports.setup_one = setup_one;
+function setup() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    setup_one('loggerPath', meta_1.default.config.loggerPath);
+}
+exports.setup = setup;
+function init(app) {
+    opts.express.app = app;
+    /* Open log file stream & initialize express logging if meta.config.logger* variables are set */
+    setup();
+}
+exports.init = init;
+function prepare_io_string(_type, _uid, _args) {
+    /*
+        * This prepares the output string for intercepted socket.io events
+        *
+        * The format is: io: <uid> <event> <args>
+        */
+    try {
+        return `io: ${_uid} ${_type} ${util.inspect(Array.prototype.slice.call(_args), { depth: 3 })}\n`;
+    }
+    catch (err) {
+        winston.info('Logger.prepare_io_string: Failed', err);
+        return 'error';
+    }
+}
+exports.prepare_io_string = prepare_io_string;
+function io_one(socket, uid) {
+    /*
+        * This function replaces a socket's .emit/.on functions in order to intercept events
+        */
+    function override(method, name, errorMsg) {
+        return (...args) => {
+            if (opts.streams.log.f) {
+                opts.streams.log.f.write(prepare_io_string(name, uid, args));
             }
-            if (client.$onevent && client.$onevent !== client.onevent) {
-                client.onevent = client.$onevent;
+            try {
+                method.apply(socket, args);
             }
+            catch (err) {
+                winston.info(errorMsg, err);
+            }
+        };
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (socket && meta_1.default.config.loggerIOStatus > 0) {
+        // courtesy of: http://stackoverflow.com/a/9674248
+        socket.oEmit = socket.emit;
+        const { emit } = socket;
+        socket.emit = override(emit, 'emit', 'Logger.io_one: emit.apply: Failed');
+        socket.$onvent = socket.onevent;
+        const $onevent = socket.onevent;
+        socket.onevent = override($onevent, 'on', 'Logger.io_one: $emit.apply: Failed');
+    }
+}
+exports.io_one = io_one;
+function io_close(socket) {
+    /*
+        * Restore all hijacked sockets to their original emit/on functions
+        */
+    if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
+        return;
+    }
+    const clientsMap = socket.io.sockets.sockets;
+    for (const [, client] of clientsMap) {
+        if (client.oEmit && client.oEmit !== client.emit) {
+            client.emit = client.oEmit;
         }
-    },
-    io: function (socket) {
-        /*
-         * Go through all of the currently established sockets & hook their .emit/.on
-         */
-        if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
-            return;
+        if (client.$onevent && client.$onevent !== client.onevent) {
+            client.onevent = client.$onevent;
         }
-        const clientsMap = socket.io.sockets.sockets;
-        for (const [, socketObj] of clientsMap) {
-            Logger.io_one(socketObj, socketObj.uid);
-        }
-    },
-    io_one: function (socket, uid) {
-        /*
-         * This function replaces a socket's .emit/.on functions in order to intercept events
-         */
-        function override(method, name, errorMsg) {
-            return (...args) => {
-                if (opts.streams.log.f) {
-                    opts.streams.log.f.write(Logger.prepare_io_string(name, uid, args));
-                }
-                try {
-                    method.apply(socket, args);
-                }
-                catch (err) {
-                    winston.info(errorMsg, err);
-                }
-            };
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (socket && meta_1.default.config.loggerIOStatus > 0) {
-            // courtesy of: http://stackoverflow.com/a/9674248
-            socket.oEmit = socket.emit;
-            const { emit } = socket;
-            socket.emit = override(emit, 'emit', 'Logger.io_one: emit.apply: Failed');
-            socket.$onvent = socket.onevent;
-            const $onevent = socket.onevent;
-            socket.onevent = override($onevent, 'on', 'Logger.io_one: $emit.apply: Failed');
-        }
-    },
-};
-exports.default = Logger;
+    }
+}
+exports.io_close = io_close;
+function io(socket) {
+    /*
+        * Go through all of the currently established sockets & hook their .emit/.on
+        */
+    if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
+        return;
+    }
+    const clientsMap = socket.io.sockets.sockets;
+    for (const [, socketObj] of clientsMap) {
+        io_one(socketObj, socketObj.uid);
+    }
+}
+exports.io = io;
+function monitorConfig(socket, data) {
+    /*
+        * This monitor's when a user clicks "save" in the Logger section of the admin panel
+        */
+    setup_one(data.key, data.value);
+    io_close(socket);
+    io(socket);
+}
+exports.monitorConfig = monitorConfig;
--- src/logger.ts
+++ src/logger.ts
@@ -44,177 +44,185 @@ type Socket = {
 
 
 /* -- Logger -- */
-const Logger = {
-     init: function (app : express.Express): void {
-        opts.express.app = app;
-        /* Open log file stream & initialize express logging if meta.config.logger* variables are set */
-        Logger.setup();
-    },
-    setup: function () {
+export function expressLogger(req : express.Request, res : express.Response, next: () => unknown) {
+    /*
+        * The new express.logger
+        *
+        * This hijack allows us to turn logger on/off dynamically within express
+        */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (meta.config.loggerStatus > 0) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        Logger.setup_one('loggerPath', meta.config.loggerPath);
-    },
-    setup_one: function (key : string, value: unknown) {
-        /*
-         * 1. Open the logger stream: stdout or file
-         * 2. Re-initialize the express logger hijack
-         */
-        if (key === 'loggerPath') {
-            Logger.setup_one_log(value as string);
-            Logger.express_open();
-        }
-    },
-    setup_one_log: function (value : string) : void {
-        /*
-         * If logging is currently enabled, create a stream.
-         * Otherwise, close the current stream
-         */
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (meta.config.loggerStatus > 0 || meta.config.loggerIOStatus) {
-            const stream : Stream = Logger.open(value);
-            if (stream) {
-                opts.streams.log.f = stream as NodeJS.WriteStream & {fd : 1};
-            } else {
-                opts.streams.log.f = process.stdout;
-            }
-        } else {
-            Logger.close(opts.streams.log);
-        }
-    },
-    open: function (value: string) {
-        /* Open the streams to log to: either a path or stdout */
-        let stream : Stream;
-        if (value) {
-            if (file.existsSync(value)) {
-                const stats = fs.statSync(value);
-                if (stats) {
-                    if (stats.isDirectory()) {
-                        stream = fs.createWriteStream(path.join(value, 'nodebb.log'), { flags: 'a' });
-                    } else {
-                        stream = fs.createWriteStream(value, { flags: 'a' });
-                    }
-                }
-            } else {
-                stream = fs.createWriteStream(value, { flags: 'a' });
-            }
-            if (stream) {
-                stream.on('error', (err) => {
-                    winston.error(err.stack);
-                });
-            }
-        } else {
-            stream = process.stdout;
-        }
-        return stream;
-    },
-    close: function (stream : Log) {
-        if (stream.f !== process.stdout && stream.f) {
-            let stream : fs.WriteStream;
-            stream.end();
-        }
-        stream.f = null;
-    },
-    monitorConfig: function (socket : Socket, data: { key: string; value: unknown; }) {
-        /*
-         * This monitor's when a user clicks "save" in the Logger section of the admin panel
-         */
-        Logger.setup_one(data.key, data.value);
-        Logger.io_close(socket);
-        Logger.io(socket);
-    },
-    express_open: function () {
-        if (opts.express.set !== 1) {
-            opts.express.set = 1;
-            opts.express.app.use(Logger.expressLogger);
-        }
-        /*
-         * Always initialize "ofn" (original function) with the original logger function
-         */
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        opts.express.ofn = morgan('combined', { stream: opts.streams.log.f });
-    },
-    expressLogger: function (req : express.Request, res : express.Response, next: () => unknown) {
-        /*
-         * The new express.logger
-         *
-         * This hijack allows us to turn logger on/off dynamically within express
-         */
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (meta.config.loggerStatus > 0) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            return opts.express.ofn(req, res, next) as unknown;
-        }
-        return next();
-    },
-    prepare_io_string: function (_type : string, _uid : string, _args : unknown[]) {
-        /*
-         * This prepares the output string for intercepted socket.io events
-         *
-         * The format is: io: <uid> <event> <args>
-         */
-        try {
-            return `io: ${_uid} ${_type} ${util.inspect(Array.prototype.slice.call(_args), { depth: 3 })}\n`;
-        } catch (err) {
-            winston.info('Logger.prepare_io_string: Failed', err);
-            return 'error';
-        }
-    },
-    io_close: function (socket : Socket) {
-        /*
-         * Restore all hijacked sockets to their original emit/on functions
-         */
-        if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
-            return;
-        }
-        const clientsMap = socket.io.sockets.sockets;
-        for (const [, client] of clientsMap) {
-            if (client.oEmit && client.oEmit !== client.emit) {
-                client.emit = client.oEmit;
-            }
-            if (client.$onevent && client.$onevent !== client.onevent) {
-                client.onevent = client.$onevent;
-            }
-        }
-    },
-    io: function (socket: Socket) : void {
-        /*
-         * Go through all of the currently established sockets & hook their .emit/.on
-         */
-        if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
-            return;
-        }
-        const clientsMap = socket.io.sockets.sockets;
-        for (const [, socketObj] of clientsMap) {
-            Logger.io_one(socketObj, socketObj.uid);
-        }
-    },
-    io_one: function (socket: Socket, uid : string) : void {
-        /*
-         * This function replaces a socket's .emit/.on functions in order to intercept events
-         */
-        function override(method: { apply: (arg0: unknown, arg1: unknown[]) => void; }, name:string, errorMsg:string) {
-            return (...args: unknown[]) => {
-                if (opts.streams.log.f) {
-                    opts.streams.log.f.write(Logger.prepare_io_string(name, uid, args));
-                }
-                try {
-                    method.apply(socket, args);
-                } catch (err) {
-                    winston.info(errorMsg, err);
-                }
-            };
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (socket && meta.config.loggerIOStatus > 0) {
-            // courtesy of: http://stackoverflow.com/a/9674248
-            socket.oEmit = socket.emit;
-            const { emit } = socket;
-            socket.emit = override(emit, 'emit', 'Logger.io_one: emit.apply: Failed');
-            socket.$onvent = socket.onevent;
-            const $onevent = socket.onevent;
-            socket.onevent = override($onevent, 'on', 'Logger.io_one: $emit.apply: Failed');
-        }
-    },
-};
+        return opts.express.ofn(req, res, next) as unknown;
+    }
+    return next();
+}
 
-export default Logger;
+export function express_open() {
+    if (opts.express.set !== 1) {
+        opts.express.set = 1;
+        opts.express.app.use(expressLogger);
+    }
+    /*
+        * Always initialize "ofn" (original function) with the original logger function
+        */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    opts.express.ofn = morgan('combined', { stream: opts.streams.log.f });
+}
+
+export function open(value: string) {
+    /* Open the streams to log to: either a path or stdout */
+    let stream : Stream;
+    if (value) {
+        if (file.existsSync(value)) {
+            const stats = fs.statSync(value);
+            if (stats) {
+                if (stats.isDirectory()) {
+                    stream = fs.createWriteStream(path.join(value, 'nodebb.log'), { flags: 'a' });
+                } else {
+                    stream = fs.createWriteStream(value, { flags: 'a' });
+                }
+            }
+        } else {
+            stream = fs.createWriteStream(value, { flags: 'a' });
+        }
+        if (stream) {
+            stream.on('error', (err) => {
+                winston.error(err.stack);
+            });
+        }
+    } else {
+        stream = process.stdout;
+    }
+    return stream;
+}
+
+export function close(stream : Log) {
+    if (stream.f !== process.stdout && stream.f) {
+        let stream : fs.WriteStream;
+        stream.end();
+    }
+    stream.f = null;
+}
+
+export function setup_one_log(value : string) : void {
+    /*
+        * If logging is currently enabled, create a stream.
+        * Otherwise, close the current stream
+        */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (meta.config.loggerStatus > 0 || meta.config.loggerIOStatus) {
+        const stream : Stream = open(value);
+        if (stream) {
+            opts.streams.log.f = stream as NodeJS.WriteStream & {fd : 1};
+        } else {
+            opts.streams.log.f = process.stdout;
+        }
+    } else {
+        close(opts.streams.log);
+    }
+}
+
+export function setup_one(key : string, value: unknown) {
+    /*
+        * 1. Open the logger stream: stdout or file
+        * 2. Re-initialize the express logger hijack
+        */
+    if (key === 'loggerPath') {
+        setup_one_log(value as string);
+        express_open();
+    }
+}
+
+export function setup() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    setup_one('loggerPath', meta.config.loggerPath);
+}
+
+export function init(app : express.Express): void {
+    opts.express.app = app;
+    /* Open log file stream & initialize express logging if meta.config.logger* variables are set */
+    setup();
+}
+
+export function prepare_io_string(_type : string, _uid : string, _args : unknown[]) {
+    /*
+        * This prepares the output string for intercepted socket.io events
+        *
+        * The format is: io: <uid> <event> <args>
+        */
+    try {
+        return `io: ${_uid} ${_type} ${util.inspect(Array.prototype.slice.call(_args), { depth: 3 })}\n`;
+    } catch (err) {
+        winston.info('Logger.prepare_io_string: Failed', err);
+        return 'error';
+    }
+}
+
+export function io_one(socket: Socket, uid : string) : void {
+    /*
+        * This function replaces a socket's .emit/.on functions in order to intercept events
+        */
+    function override(method: { apply: (arg0: unknown, arg1: unknown[]) => void; }, name:string, errorMsg:string) {
+        return (...args: unknown[]) => {
+            if (opts.streams.log.f) {
+                opts.streams.log.f.write(prepare_io_string(name, uid, args));
+            }
+            try {
+                method.apply(socket, args);
+            } catch (err) {
+                winston.info(errorMsg, err);
+            }
+        };
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (socket && meta.config.loggerIOStatus > 0) {
+        // courtesy of: http://stackoverflow.com/a/9674248
+        socket.oEmit = socket.emit;
+        const { emit } = socket;
+        socket.emit = override(emit, 'emit', 'Logger.io_one: emit.apply: Failed');
+        socket.$onvent = socket.onevent;
+        const $onevent = socket.onevent;
+        socket.onevent = override($onevent, 'on', 'Logger.io_one: $emit.apply: Failed');
+    }
+}
+
+export function io_close(socket : Socket) {
+    /*
+        * Restore all hijacked sockets to their original emit/on functions
+        */
+    if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
+        return;
+    }
+    const clientsMap = socket.io.sockets.sockets;
+    for (const [, client] of clientsMap) {
+        if (client.oEmit && client.oEmit !== client.emit) {
+            client.emit = client.oEmit;
+        }
+        if (client.$onevent && client.$onevent !== client.onevent) {
+            client.onevent = client.$onevent;
+        }
+    }
+}
+
+export function io(socket: Socket) : void {
+    /*
+        * Go through all of the currently established sockets & hook their .emit/.on
+        */
+    if (!socket || !socket.io || !socket.io.sockets || !socket.io.sockets.sockets) {
+        return;
+    }
+    const clientsMap = socket.io.sockets.sockets;
+    for (const [, socketObj] of clientsMap) {
+        io_one(socketObj, socketObj.uid);
+    }
+}
+
+export function monitorConfig(socket : Socket, data: { key: string; value: unknown; }) {
+    /*
+        * This monitor's when a user clicks "save" in the Logger section of the admin panel
+        */
+    setup_one(data.key, data.value);
+    io_close(socket);
+    io(socket);
+}
